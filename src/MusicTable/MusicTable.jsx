802941a5a628d109fb3014faa52112365f6e38d5
MPacris <mpacris@yahoo.com>
2023-05-22 15:41:58 -0500

    total running time in Minutes for the entire playlist

--- src/MusicTable/MusicTable.jsx
+++ src/MusicTable/MusicTable.jsx
@@ -1,9 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import UpdateSongModal from '../UpdateSongModal/UpdateSongModal';
 
 function MusicTable({ songs }) {
   const [selectedSong, setSelectedSong] = useState(null);
+  const [totalRunningTime, setTotalRunningTime] = useState(0);
+
+  useEffect(() => {
+    function calculateTotalRunningTime() {
+      let total = 0;
+      songs.forEach((song) => {
+        total += song.running_time;
+      });
+      setTotalRunningTime(total);
+    }
+
+    calculateTotalRunningTime();
+  }, [songs]);
 
   function handleUpdate(song) {
     setSelectedSong(song);
@@ -22,9 +35,18 @@ function MusicTable({ songs }) {
     setSelectedSong(null);
   }
 
+  function formatTime(timeInSeconds) {
+    const minutes = Math.floor(timeInSeconds / 60);
+    const seconds = timeInSeconds % 60;
+    return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+  }
+
   return (
     <div className='bottom-container'>
       <h3>Music Library</h3>
+      <div className="running-time-summary">
+        Total Running Time: {formatTime(totalRunningTime)} minutes
+      </div>
       <table className="music-table">
         <thead>
           <tr>
@@ -33,7 +55,7 @@ function MusicTable({ songs }) {
             <th>Artist</th>
             <th>Genre</th>
             <th>Release Date</th>
-            <th>Running Time</th>
+            <th>Running Time (seconds)</th>
             <th>Likes</th>
             <th>Edit</th>
             <th>Delete</th>
@@ -50,7 +72,8 @@ function MusicTable({ songs }) {
               <td>{song.running_time}</td>
               <td>{song.likes}</td>
               <td>
-                <button onClick={() => handleUpdate(song)}>Edit</button> </td>
+                <button onClick={() => handleUpdate(song)}>Edit</button>
+              </td>
               <td>
                 <button onClick={() => handleDelete(song.id)}>Delete</button>
               </td>
